db72edf2c01b9525a012229de39a33cfc915d33c
Darren Regan <darren111894@gmail.com>
2020-05-05 17:37:06 +0100

    Re-named drawable imgs

--- app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
+++ app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
@@ -2,8 +2,11 @@ package com.example.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class AdminPanelActivity : AppCompatActivity() {
+
+    private lateinit var test1:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
