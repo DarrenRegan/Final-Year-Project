af47b7af582b0da0bf9f6a757dc943c35f7d2d34
Darren Regan <darren111894@gmail.com>
2020-05-05 15:48:01 +0100

    Updating Admin Panel

---
+++ app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminPanelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_panel)
+    }
+}
