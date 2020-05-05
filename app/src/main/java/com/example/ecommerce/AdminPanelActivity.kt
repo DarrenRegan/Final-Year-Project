25468cb5d6ebff0e3b7dc284cf768893419ff8f3
Darren Regan <darren111894@gmail.com>
2020-05-05 17:52:50 +0100

    OnClickListeners for 12 Categories

--- app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
+++ app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ecommerce
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 
 class AdminPanelActivity : AppCompatActivity() {
@@ -25,5 +27,80 @@ class AdminPanelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_panel)
+
+        //*******************************************************************
+        //Layout 1 OnClickListeners - mensShirts, womansShirts, boysClothes, girlsClothes
+        //*******************************************************************
+        mensShirts.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Mens Shirts")
+            startActivity(intent)
+        }
+        womansShirts.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Womans Shirts")
+            startActivity(intent)
+        }
+        boysClothes.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Boys Clothes")
+            startActivity(intent)
+        }
+        girlsClothes.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Girls Clothes")
+            startActivity(intent)
+        }
+
+        //*******************************************************************
+        //Layout 2 OnClickListeners - womansShoes, mensShoes, bedroomFurniture, furniture
+        //*******************************************************************
+
+        womansShoes.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Womans Shoes")
+            startActivity(intent)
+        }
+        mensShoes.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Mens Shoes")
+            startActivity(intent)
+        }
+        bedroomFurniture.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Bedroom Furniture")
+            startActivity(intent)
+        }
+        furniture.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Furniture")
+            startActivity(intent)
+        }
+
+        //*******************************************************************
+        //Layout 3 OnClickListeners - tablets, phones, books, gaming
+        //*******************************************************************
+
+        tablets.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Tablets")
+            startActivity(intent)
+        }
+        phones.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Phones")
+            startActivity(intent)
+        }
+        books.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Books")
+            startActivity(intent)
+        }
+        gaming.setOnClickListener{
+            val intent = Intent(this, AdminActivity::class.java)
+            intent.putExtra("category", "Gaming")
+            startActivity(intent)
+        }
+
     }
 }
