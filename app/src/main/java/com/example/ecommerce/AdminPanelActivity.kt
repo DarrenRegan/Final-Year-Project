c77a5297796f612183e7dafe52abaa4f6f661c5f
Darren Regan <darren111894@gmail.com>
2020-05-05 17:41:26 +0100

    Update AdminPanelActivity.kt

--- app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
+++ app/src/main/java/com/example/ecommerce/AdminPanelActivity.kt
@@ -6,7 +6,21 @@ import android.widget.ImageView
 
 class AdminPanelActivity : AppCompatActivity() {
 
-    private lateinit var test1:ImageView
+    //Layout 1 @activity_admin_panel.xml
+    private lateinit var mensShirts:ImageView
+    private lateinit var womansShirts:ImageView
+    private lateinit var boysClothes:ImageView
+    private lateinit var girlsClothes:ImageView
+    //Layout 2 @activity_admin_panel.xml
+    private lateinit var womansShoes:ImageView
+    private lateinit var mensShoes:ImageView
+    private lateinit var bedroomFurniture:ImageView
+    private lateinit var furniture:ImageView
+    //Layout 3 @activity_admin_panel.xml
+    private lateinit var tablets:ImageView
+    private lateinit var phones:ImageView
+    private lateinit var books:ImageView
+    private lateinit var gaming:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
