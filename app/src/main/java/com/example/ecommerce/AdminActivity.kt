a424edab207dba2053263e785ea586834de1e8bd
Darren Regan <darren111894@gmail.com>
2020-05-05 19:55:22 +0100

    Admin Panel edit for addn items still not working

--- app/src/main/java/com/example/ecommerce/AdminActivity.kt
+++ app/src/main/java/com/example/ecommerce/AdminActivity.kt
@@ -1,7 +1,12 @@
 package com.example.ecommerce
 
+import android.app.Activity
+import android.content.Intent
+import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -10,19 +15,22 @@ import android.widget.Toast
 class AdminActivity : AppCompatActivity() {
 
     private lateinit var CategoryName:String
+    private lateinit var Desc:String
+    private lateinit var Price:String
+    private lateinit var Prod_Name:String
     private lateinit var InputProductImage:ImageView
     private lateinit var InputProductName:EditText
     private lateinit var InputProductDesc:EditText
     private lateinit var InputProductPrice:EditText
     private lateinit var AddProductButton:Button
+    private var ProductPick = 1
+    private lateinit var ImageUri:Uri
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
 
-        //Toast.makeText(this, "Welcome to Admin Panel", Toast.LENGTH_SHORT).show()
-        //Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show()
         CategoryName = getIntent().getExtras()?.get("category").toString()
 
         //Initialize Views
@@ -32,5 +40,57 @@ class AdminActivity : AppCompatActivity() {
         InputProductPrice = findViewById(R.id.product_price)
         AddProductButton =  findViewById(R.id.add_new_product)
 
+        //OnClickListeners
+        InputProductImage.setOnClickListener{
+            OpenProducts()
+        }
+
+        AddProductButton.setOnClickListener{
+            ValidateProducts()
+        }
+
+
+    }
+
+    private fun ValidateProducts() {
+        Desc = InputProductDesc.getText().toString()
+        Price = InputProductPrice.getText().toString()
+        Prod_Name = InputProductName.getText().toString()
+
+        if(ImageUri == null){
+            Toast.makeText(this, "Must add a product image!", Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(Desc)){
+            Toast.makeText(this, "Please write a Product Desc ", Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(Price)){
+            Toast.makeText(this, "Please write a Product Price ", Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(Prod_Name)){
+            Toast.makeText(this, "Please write a Product Name ", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            StoreProductInfo()
+        }
+    }
+
+    private fun StoreProductInfo() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun OpenProducts() {
+        var productIntent = Intent()
+        productIntent.setAction(Intent.ACTION_GET_CONTENT)
+        productIntent.setType("image/*")
+        startActivityForResult(productIntent, ProductPick)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == ProductPick && resultCode == RESULT_OK && data!=null){
+            ImageUri = data.getData()!!
+            InputProductImage.setImageURI(ImageUri)
+        }
     }
 }
