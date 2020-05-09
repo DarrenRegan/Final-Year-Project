c4ab42392179bf41ef6e9b615e8154b94a25ff12
Darren Regan <darren111894@gmail.com>
2020-05-09 21:13:19 +0100

    Update CartFragment.kt

--- app/src/main/java/com/example/ecommerce/ui/cart/CartFragment.kt
+++ app/src/main/java/com/example/ecommerce/ui/cart/CartFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.ecommerce.R
+import com.example.ecommerce.R.*
 
 class CartFragment : Fragment() {
 
@@ -21,7 +22,7 @@ class CartFragment : Fragment() {
     ): View? {
         cartViewModel =
             ViewModelProviders.of(this).get(CartViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_cart, container, false)
+        val root = inflater.inflate(layout.fragment_cart, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         cartViewModel.text.observe(this, Observer {
             textView.text = it
